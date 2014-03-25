249adeba1e1b59a19799c128cbb862ed3366eb3b
Sanborn <sanbornh@rogers.com>
2014-03-25 15:08:46 -0400

    updated with second list

--- exercise6.rb
+++ exercise6.rb
@@ -1,1 +1,7 @@
 grocery_list = ["carrots", "toilet paper", "apples", "salmon", "avocado", "eggs"]
+
+grocery_list.each { |item| puts "* #{item}"}
+
+grocery_list << "rice"
+
+grocery_list.each { |item| puts "* #{item}"}
