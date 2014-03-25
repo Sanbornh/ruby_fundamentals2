a84505b48569dbcc18bbe2c445c140fdf831e25a
Sanborn <sanbornh@rogers.com>
2014-03-25 15:36:28 -0400

    add check for banana method

--- exercise6.rb
+++ exercise6.rb
@@ -3,6 +3,14 @@ def print_list(list)
 	list.each { |item| puts "* #{item}"}
 end
 
+def bananas?(list)
+	if list.include?("bananas") 
+		puts "Remember, you need to pick up bananas."
+	else
+		puts "No need to pick up bananas today"
+	end
+end
+
 grocery_list = ["carrots", "toilet paper", "apples", "salmon", "avocado", "eggs"]
 
 print_list(grocery_list)
@@ -10,3 +18,4 @@ print_list(grocery_list)
 grocery_list << "rice"
 
 print_list(grocery_list)
+bananas?(grocery_list)
