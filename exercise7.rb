07e15f4c824d310b844f705a0c84636769b534f1
Sanborn <sanbornh@rogers.com>
2014-03-25 16:37:39 -0400

    Increases class size by 5%

--- exercise7.rb
+++ exercise7.rb
@@ -2,6 +2,10 @@ def print_hash(hash)
 	hash.each { |key , value| puts "#{key}: #{value} students" }
 end
 
+# Returns a hash that has increased by 5%
+def increase_size(hash)
+	hash.each { |key, value| hash[key] = (value * 1.05).round }
+end
 
 students = {
 	:cohort1 => 34,
@@ -13,3 +17,5 @@ print_hash(students)
 
 students[:cohort4] = 43
 puts students.keys
+
+print_hash(increase_size(students))
